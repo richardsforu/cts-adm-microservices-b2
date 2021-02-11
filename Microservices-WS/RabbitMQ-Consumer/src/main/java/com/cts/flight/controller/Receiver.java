c53f98267ea3e71d0a86b21ae825e005bead14f4
richardsforu <praveen.somireddy@gmail.com>
2021-02-11 17:10:28 +0530

    Microservices

--- Microservices-WS/RabbitMQ-Consumer/src/main/java/com/cts/flight/controller/Receiver.java
+++ Microservices-WS/RabbitMQ-Consumer/src/main/java/com/cts/flight/controller/Receiver.java
@@ -1,10 +1,18 @@
 package com.cts.flight.controller;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class Receiver {
+	
+	
+	@Bean
+	public Queue q1() {
+		return new Queue("TestQ",false);
+	}
 	
 	@RabbitListener(queues = {"TestQ"})
 	public void readMessage(String msg) {
