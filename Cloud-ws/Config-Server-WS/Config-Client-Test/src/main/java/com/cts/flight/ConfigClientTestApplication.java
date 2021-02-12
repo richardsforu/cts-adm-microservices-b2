0d43550fbabd8c57125c04602f5c1b65b59cc98b
richardsforu <praveen.somireddy@gmail.com>
2021-02-12 16:05:34 +0530

    Spring Boot Cloud

---
+++ Cloud-ws/Config-Server-WS/Config-Client-Test/src/main/java/com/cts/flight/ConfigClientTestApplication.java
@@ -0,0 +1,13 @@
+package com.cts.flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientTestApplication.class, args);
+	}
+
+}
